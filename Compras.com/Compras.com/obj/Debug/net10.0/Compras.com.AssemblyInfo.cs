6dd7c4df70b41b001ea7ad460ad3337be5e8c9bf
Compras-com <bielcarvalhoconstrucoesltda@gmail.com>
2026-03-28 02:57:29 +0000

    Adionando pacotes banco de dados

--- Compras.com/Compras.com/obj/Debug/net10.0/Compras.com.AssemblyInfo.cs
+++ Compras.com/Compras.com/obj/Debug/net10.0/Compras.com.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Compras.com")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bb381dcc8731315965da25272d81d93668b556a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+208e09db0d347f418345105a52ae91a20da2c0f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Compras.com")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Compras.com")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
